e0dcbbb82eabc4364cb72c1fffdcb1806cbb2521
Ohgangrae <98381603+Ohgangrae@users.noreply.github.com>
2022-03-17 13:57:22 +0900

    v

--- day8/src/am/Exam.java
+++ day8/src/am/Exam.java
@@ -12,7 +12,7 @@ public class Exam {
 		// Q1 각각 처음 수부터 숫자 2개씩을 나누어 출력하세요
 		
 //		int[] a = {1,2,3,4,5};
-		
+//		
 //		for(int i = 0; i <a.length ; i++) {
 //			if (a[i] % 2 == 0) {
 //				System.out.println(a[i]);
@@ -42,19 +42,19 @@ public class Exam {
 		//중요 시험문제
 		//Q2 첫 수부터 숫자 2개씩을 더한 값이 짝수인 그 해당 숫자 2개의 인덱스 번호를 배열로
 		// 만들어 출력하시오(처음 등장한 합한 값이 짝수인 2개의 수)
-		int[] a = {1,3,4,5,6};
-		int[] b = new int[2];
-				
-		for(int i = 0; i <a.length-1 ; i++) {
-			if((a[i]+a[i+1]) % 2 == 0) {
-				b[0]= i;
-				b[1]= i+1;
-			
-		}
-	}		
-		System.out.println(Arrays.toString(b));
-				
-		
+//		int[] a = {1,3,4,5,6};
+//		int[] b = new int[2];
+//				
+//		for(int i = 0; i <a.length-1 ; i++) {
+//			if((a[i]+a[i+1]) % 2 == 0) {
+//				b[0]= i;
+//				b[1]= i+1;
+//			
+//		}
+//	}		
+//		System.out.println(Arrays.toString(b));
+//				
+//		
 }
 }
 
